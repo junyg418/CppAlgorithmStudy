9f7248bd9a2a28b6612567eb8c2ebc634dbbb190
이준영 <fgh33678@gmail.com>
2024-03-15 23:59:04 +0900

    solve

---
+++ junyg418/bkj_class2_cpp/bkj10816.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false); 
+    cin.tie(nullptr);
+
+	map<int, int> m;
+	int N, M, n;
+	cin >> N;
+
+	while (N--)
+	{
+		cin >> n;
+		m[n]++;
+	}
+
+	cin >> M;
+	while (M--)
+	{
+		cin >> n;
+		auto it = m.find(n);
+
+		if (it != m.end())
+			cout << it->second << ' ';
+		else
+			cout << "0" << ' ';
+	}
+}
