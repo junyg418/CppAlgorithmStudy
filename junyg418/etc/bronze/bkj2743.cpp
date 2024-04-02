b25291c8a0446c5814e1db7a4b239c7b9f1358db
이준영 <fgh33678@gmail.com>
2024-04-02 23:57:29 +0900

    bronze solve

---
+++ junyg418/etc/bronze/bkj2743.cpp
@@ -0,0 +1,13 @@
+// 1차
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+
+int main(){
+    string buffer;
+    cin >> buffer;
+    cout << buffer.length();
+}
